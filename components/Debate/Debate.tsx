265ed4fad56b06369e4141672d68571b03eb7c08
Mike DeVine <pixelshredder@gmail.com>
2024-04-16 18:28:11 -0700

    add basic cooldown logic

--- components/Debate/Debate.tsx
+++ components/Debate/Debate.tsx
@@ -2,12 +2,25 @@ import NewTurn from '@/components/Turn/NewTurn'
 import { TDebate } from '@/lib/prisma-types'
 import Turns from '@/components/Turn/Turns'
 
+function checkCooldownPeriod(debate: TDebate) {
+  let lastTurn = debate.turn?.at(-1)
+
+  if (!debate.turn || !debate.turn.length || !lastTurn) {
+    return true
+  }
+
+  return (Date.now() - new Date(lastTurn.createdAt).getTime() > (60 * 100_000))
+}
+
 export default function Debate({ debate }: { debate: TDebate }) {
   return (
     <section>
       <h1>{debate.topic}</h1>
       <Turns debate={debate} />
-      <NewTurn debate={debate} />
+      { checkCooldownPeriod(debate) ?
+        <NewTurn debate={debate} /> :
+        "wait for cooldown"
+      }
     </section>
   )
 }
