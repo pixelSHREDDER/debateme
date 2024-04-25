ad7d131d0823b66f7e1015e1186278daa695e866
Mike DeVine <pixelshredder@gmail.com>
2024-04-24 18:19:40 -0700

    add basic auth0 integration

---
+++ app/user/profile/page.tsx
@@ -0,0 +1,16 @@
+import { getSession, withPageAuthRequired } from '@auth0/nextjs-auth0'
+import Image from 'next/image'
+
+export default withPageAuthRequired(async function UserProfile() {
+  const session = await getSession()
+
+  return (
+      session?.user ? (
+          <div>
+            <Image src={session.user.picture} alt={session.user.name} width={60} height={60} />
+            <h2>{session.user.name}</h2>
+            <p>{session.user.email}</p>
+          </div>
+      ) : <>Please login</>
+  )
+}, { returnTo: '/user/profile' })
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pixel-shredder.com',
+        port: '',
+        pathname: '/article-files/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pixel-shredder.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  reactStrictMode: true,
+};
 
 export default nextConfig;
